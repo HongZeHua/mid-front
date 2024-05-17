bcf30ba87f5330ef8e5ae76be61bf18bc58eddcb
Hongzehua <hzh9867811@outlook.com>
2024-05-17 22:19:45 +0800

    构建业务组件-移动端navigationBar

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'tailwindcss/tailwind.css'
 import router from './router'
+import { useREM } from './utils/flexible'
+useREM()
 createApp(App).use(router).mount('#app')
--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -8,3 +8,21 @@ const { width } = useWindowSize()
 export const isMobileTerminal = computed(() => {
   return width.value < PC_DEVICE_WIDTH
 })
+/**
+ * 初始化 rem 基准值
+ */
+export const useREM = () => {
+  //定义最大的 fontSize
+  const Max_FONT_SIZE = 40
+  //监听 html 文档被解析完成的事件
+  document.addEventListener('DOMContentLoaded', () => {
+    //获取 html 标签
+    const html = document.querySelector('html')
+    //获取根元素 fontSize 标准，屏幕宽度/10.
+    let fontSize = window.innerWidth / 10
+    //获取到的 fontSize 不允许超过我们定义的最大值
+    fontSize = fontSize > Max_FONT_SIZE ? Max_FONT_SIZE : fontSize
+    //定义根元素（html）fontSize 的大小（rem）
+    html.style.fontSize = fontSize + 'px'
+  })
+}
