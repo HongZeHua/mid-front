b845649422f8b3afd3dbed6c6e4d555d41f22c96
Hongzehua <hzh9867811@outlook.com>
2024-05-17 15:16:01 +0800

    构建移动处理工具-MobileTerminal

---
+++ src/utils/flexible.js
@@ -0,0 +1,10 @@
+import { computed } from 'vue'
+import { PC_DEVICE_WIDTH } from '../constants'
+import { useWindowSize } from '@vueuse/core'
+const { width } = useWindowSize()
+/**
+ * 判断当前是否为移动设备，判断依据是当前屏幕宽度是否小于一个指定值（1280）
+ */
+export const isMobileTerminal = computed(() => {
+  return width.value < PC_DEVICE_WIDTH
+})
