bdd05b13f605edabf2093cd036deb06651e15cbb
Hongzehua <hzh9867811@outlook.com>
2024-05-27 21:55:52 +0800

    深入vite：指令的自动注册

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,9 +1,16 @@
-import lazy from './modules/lazy'
 /**
  * 全局注册指令
  */
 export default {
   install(app) {
-    app.directive('lazy', lazy)
+    //同步引入
+    const directives = import.meta.glob('./modules/*.js', { eager: true })
+    for (const [key, value] of Object.entries(directives)) {
+      //拼接组件注册的name
+      const arr = key.split('/')
+      const directiveName = arr[arr.length - 1].replace('.js', '')
+      //完成注册
+      app.directive(directiveName, value.default)
+    }
   }
 }
